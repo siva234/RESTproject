c3041d8f0c39998f31b5ef753f4257b5796ce0f0
siva reddy <siva.agent@gmail.com>
2020-01-30 17:25:25 +0100

    Initial Setup using Spring initializr

---
+++ RESTproject/src/main/java/com/RESTproject/ResTprojectApplication.java
@@ -0,0 +1,13 @@
+package com.RESTproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTprojectApplication.class, args);
+	}
+
+}
---
+++ RESTproject/src/test/java/com/RESTproject/ResTprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RESTproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
