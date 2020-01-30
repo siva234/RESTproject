6511d597c935c44e5a9bc990e15785bcf92ed684
siva reddy <siva.agent@gmail.com>
2020-01-30 20:41:53 +0100

    minor change

--- RESTproject/src/main/java/com/RESTproject/CustomErrorController.java
+++ RESTproject/src/main/java/com/RESTproject/CustomErrorController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class CustomErrorConroller implements ErrorController {
+public class CustomErrorController implements ErrorController {
 
   @RequestMapping("/error")
   @ResponseBody
