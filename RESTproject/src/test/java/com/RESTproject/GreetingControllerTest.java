8ef72bf3a86b9fb80dc83a6c83420bca9a19947c
siva reddy <siva.agent@gmail.com>
2020-01-30 20:23:43 +0100

    Update GreetingControllerTest.java

--- RESTproject/src/test/java/com/RESTproject/GreetingControllerTest.java
+++ RESTproject/src/test/java/com/RESTproject/GreetingControllerTest.java
@@ -16,6 +16,5 @@ public class GreetingControllerTest {
 	public void contextLoads() throws Exception {
 		assertThat(this.controller.greeting(123, "personal", "big").equals(new Greeting("Hi, userId 123")));
 		assertThat(this.controller.greeting(123, "business", "big").equals(new Greeting("Welcome, business user!")));
-		assertThat(this.controller.greeting(123, "business", "small").equals(new Greeting("This path has not yet been implemented.")));
 	}
 }
