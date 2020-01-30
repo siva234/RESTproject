4cece34410ff9610d76b3a7e8659d33a113e76db
siva reddy <siva.agent@gmail.com>
2020-01-30 20:45:20 +0100

    added import statement

--- RESTproject/src/test/java/com/RESTproject/GreetingControllerTest.java
+++ RESTproject/src/test/java/com/RESTproject/GreetingControllerTest.java
@@ -1,6 +1,7 @@
 package com.RESTproject;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
