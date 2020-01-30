1b16395119ff3029eb9e28436731282f7afca91a
siva reddy <siva.agent@gmail.com>
2020-01-30 17:41:40 +0100

    Creating Service class with logic

---
+++ RESTproject/src/main/java/com/RESTproject/GreetingService.java
@@ -0,0 +1,26 @@
+package com.RESTproject;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+	
+	public GreetingService() {
+		
+	}
+	
+	public String greetingParamsLogic(Integer id,String account,String type) throws Exception {
+		if(account.equals("personal") && (id == 123))
+		{
+				return "Hi, userId "+id;
+		}
+		else if(account.equals("business") && type.equals("big"))
+		{
+				return "Welcome, "+account+" user!";
+		}
+		else
+		{
+			return  "This path has not yet been implemented.";			
+		}
+	}
+}
