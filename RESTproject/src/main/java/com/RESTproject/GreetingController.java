d50c7595480806a2c6a1fe7f548ef30aa0de1e95
siva reddy <siva.agent@gmail.com>
2021-06-07 09:59:21 +0200

    small edit

--- RESTproject/src/main/java/com/RESTproject/GreetingController.java
+++ RESTproject/src/main/java/com/RESTproject/GreetingController.java
@@ -3,11 +3,9 @@ package com.RESTproject;
 import com.RESTproject.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import javax.validation.constraints.Min;
 
 @RestController
 public class GreetingController {
@@ -16,8 +14,7 @@ public class GreetingController {
 	private GreetingService greetingservice;
 	
 	@GetMapping("/greeting")
-	@Validated
-	public ResponseEntity greet(@RequestParam(value = "id",required = false) @Min(value=0, message="Id must be a positive integer") Integer idParam ,
+	public ResponseEntity greet(@RequestParam(value = "id",required = false) Integer idParam ,
 								@RequestParam(value = "account", required = false) String accountParam,
 								@RequestParam(value = "type", required = false) String typeParam) throws Exception {
 	
